7c618752cb853387a0e32309ff43a9049e25bb46
Emmanuel Singletary <me@manny.co>
2017-11-12 18:27:01 -0500

    Added the Jira URL as a variable instead of hardcoding

--- src/providers/jira-api/jira-api.ts
+++ src/providers/jira-api/jira-api.ts
@@ -4,11 +4,14 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class JiraApiProvider {
 
+  URLString = 'https://support.mvnu.edu/rest/api/2';
+
   constructor(public http: HttpClient) {
   }
 
   authenticateUser(username, password) {
-    return this.http.get(`https://support.mvnu.edu/rest/api/2/myself`, {headers: new HttpHeaders()
+    return this.http.get(`${this.URLString}/myself`, {headers: new HttpHeaders()
     .set('Authorization', `Basic ${btoa(username+':'+password)}`)});
   }
+
 }
